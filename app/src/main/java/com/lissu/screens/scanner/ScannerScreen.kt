ae3bf0822ea8e25c5bc6080c07e077456c74f84c
recinos <00182122@uca.edu.sv>
2026-06-06 12:39:03 -0600

    style: adjusted topbar onback

--- app/src/main/java/com/lissu/screens/scanner/ScannerScreen.kt
+++ app/src/main/java/com/lissu/screens/scanner/ScannerScreen.kt
@@ -1,7 +1,9 @@
 package com.lissu.screens.scanner
 
 import android.Manifest
+import android.R.color.white
 import android.content.pm.PackageManager
+import android.graphics.Color.WHITE
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
@@ -14,12 +16,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
+import com.lissu.AppScaffold
+import com.lissu.Routes
 
 @Composable
 fun ScannerScreen(onBack: () -> Unit) {
@@ -41,81 +46,91 @@ fun ScannerScreen(onBack: () -> Unit) {
         if (!hasCameraPermission) launcher.launch(Manifest.permission.CAMERA)
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        IconButton(onClick = {
-            viewModel.resetScan()
-            onBack()
-        }) {
-            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Volver")
-        }
+    AppScaffold(
+        title = "Scanner",
+        currentScreen = Routes.Scanner,
+        navigationIcon = {
+            IconButton(onClick = {
+                viewModel.resetScan()
+                onBack()
 
-        Text("Escáner de Ítems", style = MaterialTheme.typography.headlineSmall)
-
-        // Instanciar la vista que procesa  el hardware y la IA
-        if (hasCameraPermission && !uiState.scanned) {
-            CameraBarcodeScanner(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(280.dp)
-                    .clip(RoundedCornerShape(16.dp)),
-                onBarcodeDetected = { barcode ->
-                    viewModel.onBarcodeScanned(barcode)
-                }
-            )
-        }
-
-        if (uiState.isLoading) {
-            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator()
+            }) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Volver", tint = Color.White)
             }
         }
 
-        AnimatedVisibility(visible = uiState.scanned && !uiState.isLoading) {
-            Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-                Text("Código de barras: ${uiState.barcode}", style = MaterialTheme.typography.bodyMedium)
+    ){ innerpadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerpadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
 
-                uiState.scannedItem?.let { item ->
-                    if (item.imageUrl.isNotEmpty()) {
-                        AsyncImage(
-                            model = item.imageUrl,
-                            contentDescription = "Imagen del item",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(160.dp)
-                                .clip(RoundedCornerShape(8.dp)),
-                            contentScale = ContentScale.Fit
+            Text("Escaner", style = MaterialTheme.typography.headlineSmall)
+
+            // Instanciar la vista que procesa  el hardware y la IA
+            if (hasCameraPermission && !uiState.scanned) {
+                CameraBarcodeScanner(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(280.dp)
+                        .clip(RoundedCornerShape(16.dp)),
+                    onBarcodeDetected = { barcode ->
+                        viewModel.onBarcodeScanned(barcode)
+                    }
+                )
+            }
+
+            if (uiState.isLoading) {
+                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            AnimatedVisibility(visible = uiState.scanned && !uiState.isLoading) {
+                Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                    Text("Código de barras: ${uiState.barcode}", style = MaterialTheme.typography.bodyMedium)
+
+                    uiState.scannedItem?.let { item ->
+                        if (item.imageUrl.isNotEmpty()) {
+                            AsyncImage(
+                                model = item.imageUrl,
+                                contentDescription = "Imagen del item",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(160.dp)
+                                    .clip(RoundedCornerShape(8.dp)),
+                                contentScale = ContentScale.Fit
+                            )
+                        }
+
+                        OutlinedTextField(
+                            value = item.name,
+                            onValueChange = viewModel::onNameChange,
+                            label = { Text("Nombre del Ítem") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        OutlinedTextField(
+                            value = item.category,
+                            onValueChange = viewModel::onCategoryChange,
+                            label = { Text("Categoría") },
+                            modifier = Modifier.fillMaxWidth()
                         )
                     }
 
-                    OutlinedTextField(
-                        value = item.name,
-                        onValueChange = viewModel::onNameChange,
-                        label = { Text("Nombre del Ítem") },
-                        modifier = Modifier.fillMaxWidth()
-                    )
+                    uiState.error?.let { msg ->
+                        Text(msg, color = MaterialTheme.colorScheme.error, style = MaterialTheme.typography.bodySmall)
+                    }
 
-                    OutlinedTextField(
-                        value = item.category,
-                        onValueChange = viewModel::onCategoryChange,
-                        label = { Text("Categoría") },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
-
-                uiState.error?.let { msg ->
-                    Text(msg, color = MaterialTheme.colorScheme.error, style = MaterialTheme.typography.bodySmall)
-                }
-
-                Button(
-                    onClick = viewModel::resetScan,
-                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
-                ) {
-                    Text("Escanear otro ítem")
+                    Button(
+                        onClick = viewModel::resetScan,
+                        modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+                    ) {
+                        Text("Escanear otro ítem")
+                    }
                 }
             }
         }
